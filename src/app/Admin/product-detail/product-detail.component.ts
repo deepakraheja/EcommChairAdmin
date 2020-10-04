0de76a8b15a6e8a17811de2882c655fdaeabc84d
Sonu Kumar <sonukumar18@gmail.com>
2020-10-04 17:14:52 +0530

    commit

--- src/app/Admin/product-detail/product-detail.component.ts
+++ src/app/Admin/product-detail/product-detail.component.ts
@@ -742,6 +742,10 @@ export class ProductDetailComponent implements OnInit {
     this.BannerImage = [];
     if (event.target.files && event.target.files[0]) {
       var filesAmount = event.target.files.length;
+      if(event.target.files[0].size/1024>500){
+        this._toasterService.error('Photo should be less then 500kb.');
+        return;
+      }
       for (let i = 0; i < filesAmount; i++) {
         var reader = new FileReader();
         reader.onload = (event: any) => {
@@ -761,9 +765,15 @@ export class ProductDetailComponent implements OnInit {
   }
 
   UploadSmallImg(event) {
+    debugger
     this.SmallImage = [];
     if (event.target.files && event.target.files[0]) {
       var filesAmount = event.target.files.length;
+      debugger
+      if(event.target.files[0].size/1024>500){
+        this._toasterService.error('Photo should be less then 500kb.');
+        return;
+      }
       for (let i = 0; i < filesAmount; i++) {
         var reader = new FileReader();
         reader.onload = (event: any) => {
@@ -790,6 +800,10 @@ export class ProductDetailComponent implements OnInit {
         reader.onload = (event: any) => {
           debugger
           //console.log(event.target.result);
+          if(event.total/1024>500){
+            this._toasterService.error('Photo should be less then 500kb.');
+            return;
+          }
           this.PopUpProductImg.push(event.target.result);
           this.PopUpPreviewUrl = event.target.result;
           // this.EditProductDetailForm.updateValueAndValidity();
