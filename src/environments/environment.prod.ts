db3750e54698e86ac4bc40045b9859f54fba660c
Kit <deepakrahejain@gmail.com>
2020-12-29 10:12:36 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   isContentLoading: false,
-   
+
 
   //*********************Production Server************* */
   // BASE_API_URL: 'http://208.109.12.71/EcommChairApiV3/api/',
@@ -13,11 +13,19 @@ export const environment = {
 
 
   //*********************Production Server************* */
-  BASE_API_URL: 'http://apiservice.alibabachair.com/api/',
-  ImagePath: 'http://apiservice.alibabachair.com/ProductImage/',
-  APIURL: 'http://apiservice.alibabachair.com',
-  Report_Path: 'http://apiservice.alibabachair.com/ReportGenerate/',
-  UserDocumentPath: 'http://apiservice.alibabachair.com/UserDocument/',
-  Invoice_URL: 'http://apiservice.alibabachair.com/TempPDF/',
+  // BASE_API_URL: 'http://apiservice.alibabachair.com/api/',
+  // ImagePath: 'http://apiservice.alibabachair.com/ProductImage/',
+  // APIURL: 'http://apiservice.alibabachair.com',
+  // Report_Path: 'http://apiservice.alibabachair.com/ReportGenerate/',
+  // UserDocumentPath: 'http://apiservice.alibabachair.com/UserDocument/',
+  // Invoice_URL: 'http://apiservice.alibabachair.com/TempPDF/',
+
+
+  BASE_API_URL: 'https://store.alibabachair.com/api/',
+  ImagePath: 'https://store.alibabachair.com/ProductImage/',
+  APIURL: 'https://store.alibabachair.com',
+  Report_Path: 'https://store.alibabachair.com/ReportGenerate/',
+  UserDocumentPath: 'https://store.alibabachair.com/UserDocument/',
+  Invoice_URL: 'https://store.alibabachair.com/TempPDF/',
 
 }; 
