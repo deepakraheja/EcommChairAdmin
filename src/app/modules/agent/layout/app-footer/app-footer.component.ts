0229860ee765efa70c326caf8b3d1a903209252c
Kit <deepakrahejain@gmail.com>
2021-01-10 23:51:13 +0530

    commit

--- src/app/modules/agent/layout/app-footer/app-footer.component.ts
+++ src/app/modules/agent/layout/app-footer/app-footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppFooterComponent implements OnInit {
   public year = new Date().getFullYear();
+  
   constructor() { }
 
   ngOnInit(): void {
